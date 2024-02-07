c267d479898ea7c8acc94755e1ed791c098417e6
Faizan <sfaizan78692@gmail.com>
2024-02-07 21:05:59 +0530

    footer margin fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
-  return <footer className='bg-primary py-12'>
+  return <footer className='bg-primary py-12 mt-6'>
     <div className='container mx-auto'>
       <p className='text-white text-center text-xs'>Copyright &copy; Ecommerce Shop 2024. All rights reserved.</p>
     </div>
